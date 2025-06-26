bca497f81f20f1028872611f0c0c120740b1e7c7
Douglas G. <twowheeleddev@gmail.com>
2025-06-25 22:23:04 -0400

    Update api.js

--- api.js
+++ api.js
@@ -18,3 +18,22 @@ fetchDataPromise("http://api.example.com")
   .catch((err) => {
     console.error("Error Occurred:", err);
   });
+
+// Async Await
+//* PROS = Very readable, linear control flow, straightforward try/catch
+//^ CONS = Forgetting 'await' leads to subtle bugs, error stacks can be less precise
+
+async function loadSequentially() {
+  try {
+    const result1 = await fetchDataPromise("http://api.example.com");
+    console.log("First Result:", result1.data);
+
+    const result2 = await fetchDataPromise("http://api.example.com/second");
+    console.log("Second Result:", result2.data);
+  } catch (err) {
+    console.error;
+  }
+}
+
+// Usage
+loadSequentially();
